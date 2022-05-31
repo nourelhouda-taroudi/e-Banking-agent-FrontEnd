539ef4993a8c50c407bd675f6d83624f65855a5b
fredfaya <96979843+fredfaya@users.noreply.github.com>
2022-05-31 19:22:50 +0100

    Update app.module.ts
    modification de l'app-module pour la partie agent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientFormComponent } from './Components/client-form/client-form.component';
+import { AgentLoginComponent } from './Components/agent-login/agent-login.component';
+import { FormsModule } from '@angular/forms';
+import { AgentAcceuilComponent } from './Components/agent-acceuil/agent-acceuil.component';
+import { ClientForm2Component } from './Components/client-form2/client-form2.component';
+import { AgentInfoComponent } from './Components/agent-info/agent-info.component';
+import { ListeClientsComponent } from './Components/liste-clients/liste-clients.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientFormComponent,
+    AgentLoginComponent,
+    AgentAcceuilComponent,
+    ClientForm2Component,
+    AgentInfoComponent,
+    ListeClientsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
