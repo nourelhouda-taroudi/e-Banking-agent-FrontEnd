a65a7c843f9ed6b42b1859e1a54822564958e9e2
fredfaya <96979843+fredfaya@users.noreply.github.com>
2022-06-11 14:21:06 +0100

    Update client.ts
    modification de la classe client

--- src/app/Models/client.ts
+++ src/app/Models/client.ts
@@ -1,19 +1,13 @@
 export class Client {
 
-    public id : String = new String();
+    public id : Number = new Number();
     public password : String = new String();
-    public nom : String  = new String();
-    public prenom : String = new String();
-    public naissance : Date | undefined;
+    public last_name : String  = new String();
+    public first_name : String = new String();
+    public birthday : Date | undefined;
     public email : String = new String();
-    public telephone : String = new String();
-    public adresse : String = new String();
-    public ville : String = new String();
-    public codePostal : Number | undefined;
-    public cni : String = new String();
-    public numCarteBancaire : String = new String();
-    public rib : String = new String();
-    public solde : Number | undefined;
-    public produit : String = new String();//type de compte
-    public civilite : String = new String();
+    public phone_number : String = new String();
+    public address : String = new String();
+    public idcard : String = new String();
+    public immatriculation : String = new String();
 }
